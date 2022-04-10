40f73854f998cf0df0c4c8e8eb5b13d46af26dd7
koudai0711 <t72.morishita.koudai@gmail.com>
2022-04-10 11:04:32 +0900

    createMigrate

---
+++ src/resources/views/test.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>testでござる</h1>
+<form action="/test/create" method="POST">
+    <h3>タイトル</h3>
+    <input type="text" name="title">
+    <h3>内容</h3>
+    <input type="text" name="text"><br>
+
+    <button type="submit">内容を保存しまくります</button>
+</form>
+</body>
+</html>
